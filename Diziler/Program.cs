caa0c51139d74e15606d3e8db7454585b32e4cfc
Halil Özcan <131258476+Halil-x27@users.noreply.github.com>
2024-07-08 15:52:19 +0300

    Add files via upload

---
+++ Diziler/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Diziler
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Dizi Tanımlama
+
+            string [] hayvanlar = new string[5];
+
+            string [] renkler = {"kırmızı", "mavi","beyaz"};
+
+            int [] dizi;
+            dizi = new int [5];
+
+            // Dizilere değer atama ve erişim
+
+            hayvanlar[2] = "ayı";
+            hayvanlar[4] = "kuş";
+
+            dizi [0] = 10;
+            dizi [1] = 15;
+
+            Console.WriteLine(hayvanlar[2]);
+            Console.WriteLine(renkler[2]);
+            Console.WriteLine(dizi[1]);
+
+
+            //Döngülerle dizi kullanımı
+
+            Console.WriteLine("Dizi Uzunluğunu Giriniz");
+            int diziUzunlugu = int.Parse(Console.ReadLine());
+            int [] sayiDizisi = new int [diziUzunlugu];
+
+            for (int i = 0; i < diziUzunlugu; i++)
+            {
+                Console.WriteLine("dizinin {0}. elemanını giriniz", i+1);
+                sayiDizisi[i] = int.Parse(Console.ReadLine());
+                
+            }
+
+            int toplam = 0;
+            foreach (var sayi in sayiDizisi)
+            {
+                toplam+= sayi;
+
+            }
+
+            Console.WriteLine(toplam/diziUzunlugu);
+
+
+
+
+        }
+    }
+}
